da6baf3f806ec12af2a3c642fff76c8063e765e8
Pranay Agrawal <100509183+Pranay38@users.noreply.github.com>
2024-06-19 20:29:31 +0530

    Time: 509 ms (55.73%) | Memory: 202.9 MB (67.31%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,32 +1,31 @@
 class Solution {
-#define ll long long 
-private:
-    ll f(vector<ll>& dp, map<int, int>& frequencyMap, vector<int>& uniquePowers, int index) {
-        if (index == uniquePowers.size()) return 0;
-        if (dp[index] != -1) return dp[index];
-       
-        ll skip = f(dp, frequencyMap, uniquePowers, index + 1);
-        
-        ll take = 0;
-        int nextIndex = index + 1;
-        while (nextIndex < uniquePowers.size() && uniquePowers[nextIndex] - uniquePowers[index] <= 2) {
-            nextIndex++;
-        }
-        take = ((1ll)*frequencyMap[uniquePowers[index]] * uniquePowers[index]) + f(dp, frequencyMap, uniquePowers, nextIndex);
-        return dp[index] = max(take, skip);
-    }
+    #define ll long long
 public:
+ll f(vector<ll>&dp,vector<int>&power,unordered_map<int,int>&mp,vector<int>&t,int ind){
+    int n=power.size();
+    if(ind>=n)
+    return 0;
+    if(dp[ind]!=-1)
+    return dp[ind];
+    ll notpick=f(dp,power,mp,t,ind+1);
+    ll pick=0;
+    ll nind=ind+1;
+    while(nind<n&&power[nind]-power[ind]<=2)
+    nind++;
+    pick=(1ll)*mp[t[ind]]*t[ind]+f(dp,power,mp,t,nind);
+    return dp[ind]=max(pick,notpick);
+
+}
     long long maximumTotalDamage(vector<int>& power) {
-        map<int, int> frequencyMap;
-        vector<int> uniquePowers;
-        for (auto p : power) {
-            frequencyMap[p]++;
+        vector<int>t(power);
+        sort(t.begin(),t.end());
+        t.resize(unique(t.begin(),t.end())-t.begin());
+        unordered_map<int,int>mp;
+        for(auto it:power){
+            mp[it]++;
         }
-        for (auto it : frequencyMap) {
-            uniquePowers.push_back(it.first);
-        }
-        vector<ll> dp(frequencyMap.size(), -1);
-       
-        return f(dp, frequencyMap, uniquePowers, 0);
+        vector<ll>dp(t.size(),-1);
+        return f(dp,t,mp,t,0);
+
     }
 };
