751cbf1f1f16696d4ee70c6283dd260d3cb1cbd6
Pranay Agrawal <100509183+Pranay38@users.noreply.github.com>
2025-01-07 12:06:30 +0530

    Time: 1908 ms (28.67%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 with cte as(
-    select e.id,e.name,u.unique_id
-    from Employees e
+    select e.id,e.name,u.unique_id from employees e
     left join EmployeeUNI u on e.id=u.id
 )
 select c.unique_id,c.name from cte c
+order by c.unique_id
